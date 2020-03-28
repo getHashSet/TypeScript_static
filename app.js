ef6aea4627db50e7ab977cbb18e7b6cba3ffd5d7
Matthew <matthew_carpenter@icloud.com>
2020-03-28 15:10:50 -0700

    updated.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var button = document.querySelector('button');
-var number1 = document.getElementById('input1');
-var number2 = document.getElementById('input2');
-var modernJavascript = function (num, num2) {
-    return num + num2;
+var button = document.querySelector("button");
+var number1 = document.getElementById("input1");
+var number2 = document.getElementById("input2");
+var modernJavascript = function(num, num2) {
+  return num + num2;
 };
-button.addEventListener("click", function () {
-    console.log(modernJavascript(+number1.value, +number2.value));
+
+button.addEventListener("click", function() {
+  console.log(modernJavascript(+number1.value, +number2.value));
 });
--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
-const button = document.querySelector('button');
-const number1 = document.getElementById('input1')! as HTMLInputElement;
-const number2 = document.getElementById('input2')! as HTMLInputElement;
+const button = document.querySelector("button");
+const number1 = document.getElementById("input1")! as HTMLInputElement;
+const number2 = document.getElementById("input2")! as HTMLInputElement;
 
 const modernJavascript = (num: number, num2: number) => {
-    return num + num2;
+  return num + num2;
 };
 
 button.addEventListener("click", function() {
-    console.log(modernJavascript(+number1.value, +number2.value));
+  console.log(modernJavascript(+number1.value, +number2.value));
 });
